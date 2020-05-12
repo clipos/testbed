clipos/testbed

commit 69b5cd2c757c53403d9d1e1d77340c82c48c56b1

boxes: Keep only Debian testing setup in first step
Move package installation to second provisioning stage as rebuilding the
base box is time consuming as it performs a full Debian stable to
testing update.

Change-Id: Idf9515e6beee7bf937a83ab77b45a40c391544e4
Signed-off-by: Timothée Ravier <timothee.ravier@ssi.gouv.fr>

provisioning/ipsec-gw.sh
@@ -4,6 +4,13 @@
 
 set -eu -o pipefail
 
+# Set appropriate hostname
+echo " [*] Setup hostname to: '${HOSTNAME}'..."
+HOSTNAME="ipsec-gw"
+hostnamectl set-hostname "${HOSTNAME}"
+echo "${HOSTNAME}" > /etc/hostname
+echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
+
 echo " [*] Fix networkd configuration..."
 for f in "50-vagrant-ens7.network" "99-dhcp.network"; do
     install -v -o 0 -g 0 -m 0644 "/vagrant/networkd/${f}" "/etc/systemd/network/${f}"
@@ -11,6 +18,19 @@ done
 
 echo " [*] Restart systemd-networkd & systemd-resolved services..."
 systemctl restart systemd-networkd systemd-resolved
+
+# Update both packages index and installed packages
+apt-get -y -q update
+apt-get -y -q dist-upgrade
+
+# Install:
+#   - strongSwan (with swanctl utilities and systemd interfacing)
+#   - nftables (firewall)
+#   - nginx (update server)
+apt-get -y -q install \
+    charon-systemd \
+    nftables \
+    nginx
 
 echo " [*] Install the dummy IPsec PKI..."
 install -v -o 0 -g 0 -m 0644 "/vagrant/pki/root-ca.cert.pem" "/etc/swanctl/x509ca/root-ca.cert.pem"
@@ -68,9 +88,6 @@ systemctl enable --now netns@ipsec0.service
 echo " [*] Restart strongSwan service..."
 systemctl daemon-reload
 systemctl restart strongswan.service
-
-echo " [*] Setup hostname in /etc/hosts..."
-echo "127.0.0.1 ipsec-gw" >> /etc/hosts
 
 echo " [*] Install nginx configuration for updates..."
 for f in "update.clip-os.org.conf" "update.clip-os.org-key.pem" "update.clip-os.org.pem"; do

Vagrantfile
@@ -17,9 +17,9 @@ Vagrant.configure("2") do |config|
     libvirt.management_network_mode = "none"  # Do not NAT this bridge interface
   end
 
-  # IPsec gateway with our image created with Packer
+  # IPsec gateway based on Debian testing image
   config.vm.define "ipsec-gw" do |ipsecgw|
-    ipsecgw.vm.box = "clipos-testbed/ipsec-gw"
+    ipsecgw.vm.box = "clipos-testbed/debian"
 
     # Main local network interface with internet access. CLIP OS workstations
     # and gateway will be connected to this network.

boxes/Vagrantfile
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # Copyright © 2019 ANSSI. All rights reserved.
 
-# Vagrantfile to create the "clipos-testbed/ipsec-gw" box.
+# Vagrantfile to create the "clipos-testbed/debian" box.
 
 ENVNAME = "build_clipos-testbed"
 
@@ -20,8 +20,8 @@ Vagrant.configure("2") do |config|
   # Disable default synced_folder configuration
   config.vm.synced_folder ".", "/vagrant", disabled: true
 
-  config.vm.define "ipsec-gw" do |ipsecgw|
-    ipsecgw.vm.box = "debian/buster64"
+  config.vm.define "debian" do |debian|
+    debian.vm.box = "debian/buster64"
 
     # Do not automatically change the SSH keypair for a newly generated key pair
     # otherwise we would need to re-embed the default insecure Vagrant SSH key
@@ -33,7 +33,7 @@ Vagrant.configure("2") do |config|
     config.ssh.insert_key = false
 
     # Provider-specific settings for this VM:
-    ipsecgw.vm.provider "libvirt" do |libvirt|
+    debian.vm.provider "libvirt" do |libvirt|
       libvirt.cpus = 4
       libvirt.cputopology sockets: "1", cores: "2", threads: "2"
       libvirt.memory = 2048
@@ -42,11 +42,10 @@ Vagrant.configure("2") do |config|
     end
 
     # rsync the /vagrant folder rather than using NFS
-    ipsecgw.vm.synced_folder "./synced_folders/ipsec-gw", "/vagrant/", type:
-      "rsync"
+    debian.vm.synced_folder ".", "/vagrant/", type: "rsync"
 
     # Provisioning:
-    ipsecgw.vm.provision "shell", path: "provisioning/ipsec-gw.sh"
+    debian.vm.provision "shell", path: "debian.sh"
   end
 
 end

boxes/debian.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # Copyright © 2019 ANSSI. All rights reserved.
 
-# Provisioning script for the Debian IPsec gateway
+# Provisioning script to create a Debian testing Vagrant box
 
 set -eu -o pipefail
 
@@ -18,19 +18,8 @@ sed -i 's|buster|testing|g' /etc/apt/sources.list
 apt-get -y -q update
 apt-get -y -q dist-upgrade
 
-# Install:
-#   - strongSwan (with swanctl utilities and systemd interfacing)
-#   - nftables (firewall)
-#   - nginx (update server)
-#   - supplementary handy tools for developer/tester convenience
-apt-get -y -q install \
-    charon-systemd \
-    nftables \
-    nginx \
-    vim bash-completion tmux openssl tree htop
-
-# Set appropriate hostname
-echo "ipsec-gw" > /etc/hostname
+# Install supplementary handy tools for developer/tester convenience
+apt-get -y -q install vim bash-completion tmux openssl tree htop
 
 # Installs the sshd-keygen oneshot service. This oneshot unit is required to
 # re-generate the SSHD host keys that are automatically deleted when packing

build_vagrant_boxes.sh
@@ -22,7 +22,7 @@ main() {
 
     # Boxes to build:
     local -r boxes=(
-        'ipsec-gw'
+        'debian'
     )
 
     for box in "${boxes[@]}"; do

setup_testbed.sh
@@ -15,12 +15,27 @@ fi
 readonly PROGNAME="${BASH_SOURCE[0]##*/}"
 readonly PROGPATH="$(realpath "${BASH_SOURCE[0]%/*}")"
 
+# Latest Debian Testing box version available from the public CI
+BOX_VERSION="20200324"
+
 main() {
     pushd "${PROGPATH}" > /dev/null
 
-    if [[ -z "$(vagrant box list 2>/dev/null | grep "clipos-testbed/ipsec-gw" | cut -f1 -d\ )" ]]; then
-        echo "[+] Building Vagrant boxes..."
-        ./build_vagrant_boxes.sh
+    if [[ -z "$(vagrant box list 2>/dev/null | grep "clipos-testbed/debian" | cut -f1 -d\ )" ]]; then
+        if [[ -z "$(command -v cosmk)" ]]; then
+            >&2 echo "[!] Could not find \"cosmk\". Aborting."
+            exit 1
+        fi
+
+        local -r url="$(cosmk ci-artifacts)"
+        if [[ -n "${url}" ]]; then
+            echo "[+] Downloading Vagrant box from ${url}..."
+            vagrant box add --name "clipos-testbed/debian" \
+                "${url}/00_vagrant_boxes/clipos-testbed_debian.${BOX_VERSION}.box"
+        else
+            echo "[+] Building Vagrant boxes..."
+            ./build_vagrant_boxes.sh
+        fi
     fi
 
     vagrant up

commit 3d5b8bb07b0bb61f172ef04dda4048098485a395

ipsec-gw: Make sure provisioning is non-interactive
Otherwise, annoying things like terminal encoding changes can happen.
While at it, set LANG as in boxes/debian.sh.

Fixes: 69b5cd2c757c ("boxes: Keep only Debian testing setup in first step")
Change-Id: Ia022682af09e7378ed79e50fb0155707dbb61339
Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>

provisioning/ipsec-gw.sh
@@ -4,6 +4,9 @@
 
 set -eu -o pipefail
 
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C.UTF-8
+
 # Set appropriate hostname
 echo " [*] Setup hostname to: '${HOSTNAME}'..."
 HOSTNAME="ipsec-gw"